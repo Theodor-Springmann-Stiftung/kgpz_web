09d900b5b3b13288b8fc22d29c71347504aec9e6
Simon Martens <simon.martens@mailbox.org>
2024-12-04 13:11:49 +0100

    logo fix

--- views/tailwind.config.js
+++ views/tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./routes/**/*.{html,js,svelte,ts,tmpl,gotmpl,gohtml}",
-    "./transform/**/*.{html,js,svelte,ts,tmpl,gotmpl,gohtml}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        script: ["Rancho", "ui-serif"],
-        sans: ['"Source Sans 3"', '"Merriweather Sans"', "ui-sans-serif"],
-        serif: ['"Merriweather"', "ui-serif"],
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./routes/**/*.{html,js,svelte,ts,tmpl,gotmpl,gohtml}",
+		"./layouts/**/*.{html,js,svelte,ts,tmpl,gotmpl,gohtml}",
+		"./transform/**/*.{html,js,svelte,ts,tmpl,gotmpl,gohtml}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				script: ["Rancho", "ui-serif"],
+				sans: ['"Source Sans 3"', '"Merriweather Sans"', "ui-sans-serif"],
+				serif: ['"Merriweather"', "ui-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
