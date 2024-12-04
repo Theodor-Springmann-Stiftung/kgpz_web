34ca6c15bf2804843b44f0862cc76e1a04d7066b
Simon Martens <simon.martens@mailbox.org>
2024-12-04 17:28:56 +0100

    Fixed some build options for vite

--- views/vite.config.js
+++ views/vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	build: {
 		root: resolve(__dirname, ""),
+		// These are dev options only:
+		assetsInlineLimit: 0,
+		minify: false,
 		lib: {
 			entry: "./transform/main.js",
 			name: "PC-UI",
